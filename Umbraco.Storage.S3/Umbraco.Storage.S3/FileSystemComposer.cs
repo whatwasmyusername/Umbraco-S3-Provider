5426c02a898db30e94ddc0fb1ca7bad72b09915c
Joe Delly <joe@ipsocreative.com>
2019-03-26 14:18:50 -0400

    Added method to switch between cached and non-cached file systems

--- Umbraco.Storage.S3/Umbraco.Storage.S3/FileSystemComposer.cs
+++ Umbraco.Storage.S3/Umbraco.Storage.S3/FileSystemComposer.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 using Umbraco.Core.Composing;
-using Umbraco.Core.Composing.CompositionExtensions;
-using Umbraco.Core;
-using Umbraco.Storage.S3.Services.Impl;
 using Umbraco.Core.IO;
-using System.Configuration;
 
 namespace Umbraco.Storage.S3
 {
@@ -22,16 +14,40 @@ namespace Umbraco.Storage.S3
 			bucketKeyPrefix = ConfigurationManager.AppSettings["MediaFileSystem.BucketKeyPrefix"];
 			region = ConfigurationManager.AppSettings["MediaFileSystem.Region"];
 			cachePath = ConfigurationManager.AppSettings["MediaFileSystem.CachePath"];
-			timeToLive = ConfigurationManager.AppSettings["MediaFileSystem.TimeToLive"];
 			cannedACL = ConfigurationManager.AppSettings["MediaFileSystem.CannedACL"];
 			serverSideEncryptionMethod = ConfigurationManager.AppSettings["MediaFileSystem.ServerSideEncryptionMethod"];
+			timeToLive = ConfigurationManager.AppSettings["MediaFileSystem.TimeToLive"];
 			if (string.IsNullOrEmpty(bucketHostName))
 			{
 				return;
 			}
-			CachedBucketFileSystem fs = new CachedBucketFileSystem(bucketName, bucketHostName, bucketKeyPrefix, region, cachePath, timeToLive, cannedACL, serverSideEncryptionMethod);
+			IFileSystem fs;
+			if (string.IsNullOrEmpty(timeToLive) || string.IsNullOrEmpty(cachePath))
+			{
+				fs = new BucketFileSystem(
+					bucketName: bucketName,
+					bucketHostName: bucketHostName,
+					bucketKeyPrefix: bucketKeyPrefix,
+					region: region,
+					cannedACL: cannedACL,
+					serverSideEncryptionMethod: serverSideEncryptionMethod
+					);
+			}
+			else
+			{
+				fs = new CachedBucketFileSystem(
+					bucketName: bucketName,
+					bucketHostName: bucketHostName,
+					bucketKeyPrefix: bucketKeyPrefix,
+					region: region,
+					cachePath: cachePath,
+					timeToLive: timeToLive,
+					cannedACL: cannedACL,
+					serverSideEncryptionMethod: serverSideEncryptionMethod
+					);
+			}
 			composition.RegisterUniqueFor<IFileSystem, IMediaFileSystem>(fs);
-			composition.Logger.Info(typeof(FileSystemComposer), "Cached Bucket File System Setup: Bucket Name: {0}, Host Name: {1}, Region: {2}", bucketName, bucketHostName, region);
+			composition.Logger.Info(typeof(FileSystemComposer), "Bucket File System Setup: Bucket Name: {0}, Host Name: {1}, Region: {2}, Type: {3}", bucketName, bucketHostName, region, fs.GetType());
 		}
 	}
 }
